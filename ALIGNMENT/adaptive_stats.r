64364992c598c834b43c280dbd14f50973400908
Cuspist <40503215+Cuspist@users.noreply.github.com>
2021-12-07 15:17:22 +0000

    added function

--- ALIGNMENT/adaptive_stats.r
+++ ALIGNMENT/adaptive_stats.r
@@ -6,37 +6,41 @@ require(tidyverse)
 
 args = commandArgs(trailingOnly = TRUE)
 #print(args)
-
-adaptive_seq <- read.csv(args[1])
+#adaptive_seq <- read.csv(args[1])
 #print(head(adaptive_seq))
 
-#tally adaptive seq descisions
-counts <- count(adaptive_seq, decision) %>%
-          mutate(pct = n/sum(n)*100)
+adaptive_stats <- function(adaptive_seq, run_name){
 
-write.table(counts, "adaptive_seq_stats.txt", sep = " ", row.names = F)
+  #tally adaptive seq descisions
+  counts <- count(adaptive_seq, decision) %>%
+            mutate(pct = n/sum(n)*100)
 
-#split by decision
-decisions <- adaptive_seq %>%
-  split(f = as.factor(.$decision))
+  write.table(counts, "adaptive_seq_stats.txt", sep = " ", row.names = F, quote = F)
 
-#save reads with each decision - python script takes whole adaptive output file, but may be useful to have separated files....
-# for(dec in names(decisions)){
-#   x <- decisions[[dec]]
-#   write.table(x, paste(args[2], dec, "read.txt", sep = "_"),
-#               quote = FALSE,
-#               sep = "\t",
-#               row.names = FALSE)
-#                       }
+  #split by decision
+  decisions <- adaptive_seq %>%
+    split(f = as.factor(.$decision))
 
-#plot read descisions
-read_count_plot <- ggplot(counts, aes(y = n, x = decision, label = round(pct, 2))) +
-      geom_bar(stat = 'identity') +
-      geom_label() +
-      labs(title = paste(args[2]),
-            x = "Decision",
-            y = "Number of reads")
+  #save reads with each decision - python script takes whole adaptive output file, but may be useful to have separated files....
+  # for(dec in names(decisions)){
+  #   x <- decisions[[dec]]
+  #   write.table(x, paste(args[2], dec, "read.txt", sep = "_"),
+  #               quote = FALSE,
+  #               sep = "\t",
+  #               row.names = FALSE)
+  #                       }
 
-ggsave(paste0(args[2], "_adaptive_stats.pdf"), read_count_plot)
+  #plot read descisions
+  read_count_plot <- ggplot(counts, aes(y = n, x = decision, label = round(pct, 2))) +
+        geom_bar(stat = 'identity') +
+        geom_label() +
+        labs(title = paste(run_name),
+              x = "Decision",
+              y = "Number of reads")
+
+  ggsave(paste0(run_name, "_adaptive_stats.pdf"), read_count_plot)
+  }
+
+adaptive_stats(read.csv(args[1]), args[2])
 
 quit(save = "no")
