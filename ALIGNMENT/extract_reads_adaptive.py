89063ffe58bcf3ee3836fbdd94af8a26e100cc3f
Cuspist <m.v.salmon@soton.ac.uk>
2022-01-14 16:33:40 +0000

    print number of reads

--- ALIGNMENT/extract_reads_adaptive.py
+++ ALIGNMENT/extract_reads_adaptive.py
@@ -14,7 +14,7 @@ def get_reads(adaptive_output):
     #Added by MS
     #iterate over adaptive output file and store each read_id in dict {decision:[id1, id2, id3...]}
     read_dict = {}
-    with open(adaptive_output) as f:
+    with open(adaptive_output, 'r') as f:
         #skip header row
         for head in range(1):
             next(f)
@@ -26,7 +26,8 @@ def get_reads(adaptive_output):
                 read_dict[decision] = [read_id]
             else:
                 read_dict[decision].append(read_id)
-
+    for n in read_dict:
+        print("{0} reads = {1}".format(n, len(read_dict[n])))
     return read_dict
 
 
@@ -81,5 +82,5 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--adaptive_output', help='adaptive sampling output file', required=False)
     parser.add_argument('-o', '--out', help='file name for extracted alignments', required=True)
     options = parser.parse_args()
-    
+
     extract_reads(options)
