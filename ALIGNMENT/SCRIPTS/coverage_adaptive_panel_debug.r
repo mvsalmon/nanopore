b7ff21122394e0413598f73f6f7a18812a6ecf7a
Matt Salmon <m.v.salmon@soton.ac.uk>
2023-04-20 15:32:39 +0100

    fixed PAR1 region coverage

--- ALIGNMENT/SCRIPTS/coverage_adaptive_panel_debug.r
+++ ALIGNMENT/SCRIPTS/coverage_adaptive_panel_debug.r
@@ -1,6 +1,7 @@
 library(tidyverse)
 
-args = c("E16023_combined")
+#run_name = c("E16023_combined_chr_grp")
+args = commandArgs(trailingOnly = TRUE)
 #1) run name
 
 # Coverage Function -------------------------------------------------------
@@ -8,43 +9,62 @@ args = c("E16023_combined")
 coverage <- function(cov_file, run_name, save_dir){
   #calculate depth & coverage for each target in coverage file
 
-  #  cov_data <- cov_file %>%
-  #    group_by(gene)
-
+  #summary of overall sequencing depth
+  depth_summary <- cov_file %>% 
+    ungroup() %>%
+    summarise(MinDepth = min(depthAtPos),
+              MaxDepth = max(depthAtPos),
+              MedianDepth = median(depthAtPos),
+              MeanDepth = mean(depthAtPos),
+              SD = sd(depthAtPos))
+  write_delim(depth_summary,
+              sprintf("%s_depth_summary.tsv", run_name),
+              delim = "\t",
+              quote = "none")
+  
+  #per-gene summary
+  per_gene_depth_summary <- cov_file %>% 
+    group_by(gene, chrom) %>%
+    summarise(MinDepth = min(depthAtPos),
+              MaxDepth = max(depthAtPos),
+              MedianDepth = median(depthAtPos),
+              MeanDepth = mean(depthAtPos),
+              SD = sd(depthAtPos))
+  write_delim(per_gene_depth_summary,
+              sprintf("%s_per_gene_depth_summary.tsv", run_name),
+              delim = "\t",
+              quote = "none")
+  
   #count bases with non-zero coverage
   coverage_data <- cov_file %>%
     filter(depthAtPos != 0) %>%
     tally(name = 'coveredBases')
 
-    # if(str_detect(coverage$gene, "^ENST")){
-    #   coverage <- coverage %>%
-    #     separate(gene, into = c('Transcript_ID', 'gene'), sep = '_')
-    # }
 
   #depth and coverage calculations
   depth_table <- cov_file %>%
     summarise(featureLength = n(),
               #mean of per-base depth for a feature
-              meanDepth = mean(depthAtPos),
+              medianDepth = median(depthAtPos),
               ) %>%
     #join coverage data
     left_join(coverage_data) %>%
     #calculate fractional coverage of target. 1 = all bases in target present in at least 1 read
     mutate(fractionalCoverage = coveredBases/featureLength) %>%
     #separate gene name and RefSeq accession
-    separate(gene, into = c('gene', 'RefSeq'), sep = ',') #%>%
-    #output tablhttp://127.0.0.1:30185/graphics/plot_zoom_png?width=1200&height=900e with per-target depth and coverage info
-    # write_delim(sprintf("%s_coverage_data.tsv", run_name),
-    #             delim = "\t",
-    #             quote = "none")
+    separate(gene, into = c('gene', 'RefSeq'), sep = ',') %>%
+    #output table with per-target depth and coverage info
+    write_delim(sprintf("%s_coverage_data.tsv", run_name),
+                delim = "\t",
+                quote = "none")
 
   #summary of coverage data
 
   #plot depth per target
-  depth_plot <- ggplot(depth_table, aes(x = gene, y = meanDepth)) +
+  depth_plot <- ggplot(depth_table, aes(x = gene, y = medianDepth)) +
     geom_bar(stat = 'identity') +
     labs(title = sprintf("%s mean depth", run_name),
-         y = 'Mean Depth') +
+         y = 'Median Depth') +
     theme(axis.text.x = element_text(size = 4)) +
     scale_x_discrete(guide = guide_axis(n.dodge = 3, angle = 45))
 
@@ -68,11 +88,11 @@ coverage <- function(cov_file, run_name, save_dir){
                         delim = "\t",
                         col_names = c("chrom", "chromStart", "chromEnd", "gene", "score", "strand", "basePos", "depthAtPos")
                         ) %>%
-                        
-    group_by(gene)
+    #grouping by chrom accounts for PAR1 on X/Y where no mapping to Y makes a 
+    #few genes in this region appear to only have half the depth/coverage
+    #chrom group gives a row for each gene on X and Y
+    group_by(gene, chrom)
 
   coverage(cov_file, args[1], getwd())
-
-
 
 quit(save = "no")
